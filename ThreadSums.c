c7d1472b0a451c8986772b22038f57fa57b4eb7d
2018182027 <ge18b027@computer.kpu.ac.kr>
2020-06-09 22:32:05 +0900

    수정완료
    `

--- ThreadSums.c
+++ ThreadSums.c
@@ -3,7 +3,7 @@
 //All results are passed from the thread to main and printed there.
 //To compile: gcc ThreadedFactorialSums.c -lpthread
 #include <pthread.h>
-#include <stdio.h>
+#include <studio.h>
 #include <stdlib.h>
 #define NUMT 8 //Num threads
 
@@ -11,35 +11,20 @@
 //to pass and return them to the threads. 
 void *process(void* param);
 long fact(long num);
-long sum(long num);
 //-----------------------------------------------------------
 //-----------------------------------------------------------
 void *process( void* param ){ //Function where the threads begin and split based on odd/even thread IDs (1-8).
 
 	long num = 0;
-	//printf("myid: %ld\n", (long)param); //Display the thread IDs.
-
-	if((long)param % 2 == 1){//Odd threads. Find factorial.
-		num = fact((long)param);
-	}
-	else{//Even threads. Find summation.
-		num = sum((long)param);
-	}
-
-	pthread_exit( (void*) num ); //Stop the thread after work is complete and send return value.
+	printf("myid: %ld\n", (long)param); //쓰레드 아이디.
+	num = fact((long)param);
+	pthread_exit( (void*) num );
 }
 
-long fact(long num){ //Return factorial from 1 to num (iterative function, not recursive).
+long fact(long num){
 	long result = 1;
 	for(int i = 2; i <= num; i++){
         result *= i;
-    }
-	return result;
-}
-long sum(long num){ //Return summation from 1 to num.
-	long result = 0;
-	for(int i = 0; i <= num; i++){
-        result += i;
     }
 	return result;
 }
@@ -49,21 +34,19 @@ int main(){
 	pthread_t tid[NUMT]; //store pthread IDs in array
 	void *status;
 	long i, sum = 0;
-	//printf("In main: time to build threads\n");
 
 	for(i = 1; i <= NUMT; i++){
-		//printf("Creating %ld\n", i);
-		pthread_create(&tid[i - 1], NULL, process, (void *)i); //type cast i to a void pointer and start the threads in the process function.
+	printf("Creating %ld\n", i);
+	pthread_create(&tid[i - 1], NULL, process, (void *)i); //쓰레드 생성
 	}
 
-	//printf("In main: all threads created\n");
+        printf("In main: all threads created\n");
 
 	for(i = 1; i <= NUMT; i++){
-		//printf(" Main: joined %ld\n", i);
-		pthread_join(tid[i - 1], &status); //Waits on thread ID tid[i] (0 to 8 in order) to finish before moving on. Store the return value in status.
+		printf(" Main: joined %ld\n", i);
+		pthread_join(tid[i - 1], &status);
 		printf("Thread %ld returned: %ld\n", i, (long)status);
-        sum += (long)status;
 	}
-    printf("Total of all results: %ld\n", sum);
+ 
     return 0;
 }
